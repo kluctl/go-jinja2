30167c5cbd9a9ddce62bbfac35169f9c0e3d19c7
Alexander Block <ablock84@gmail.com>
2022-09-06 23:33:01 +0200

    feat: Implement RenderStruct

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kluctl/go-jinja2
 go 1.19
 
 require (
+	github.com/hashicorp/go-multierror v1.1.1
 	github.com/jinzhu/copier v0.3.5
 	github.com/kluctl/go-embed-python v0.0.0-3.10.6-20220906-2
 	github.com/stretchr/testify v1.7.2
@@ -12,6 +13,7 @@ require (
 	github.com/cyphar/filepath-securejoin v0.2.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.1 // indirect
--- jinja2_test.go
+++ jinja2_test.go
@@ -7,12 +7,20 @@ import (
 	"testing"
 )
 
-func TestJinja2(t *testing.T) {
+func newJinja2(t *testing.T) *Jinja2 {
 	name := fmt.Sprintf("jinja2-%d", rand.Uint32())
 	j2, err := NewJinja2(name, 1, WithGlobal("test_var1", 1), WithGlobal("test_var2", map[string]any{"test": 2}))
-	assert.NoError(t, err)
-	defer j2.Close()
-	defer j2.Cleanup()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(j2.Close)
+	t.Cleanup(j2.Cleanup)
+
+	return j2
+}
+func TestJinja2(t *testing.T) {
+	j2 := newJinja2(t)
 
 	s, err := j2.RenderString("test")
 	assert.NoError(t, err)
@@ -33,4 +41,63 @@ func TestJinja2(t *testing.T) {
 	s, err = j2.RenderString("test - {{ get_var('test_var2.test1', 'd') }}", WithExtension("go_jinja2.ext.kluctl"))
 	assert.NoError(t, err)
 	assert.Equal(t, "test - d", s)
+}
+
+type testStruct struct {
+	V1 string         `json:"v1"`
+	S1 testStruct2    `json:"s1"`
+	M1 map[string]any `json:"m1"`
+}
+
+type testStruct2 struct {
+	V2 string `json:"v2"`
+}
+
+func TestRenderStruct(t *testing.T) {
+	j2 := newJinja2(t)
+
+	s := testStruct{
+		V1: `{{ "1" }}`,
+		S1: testStruct2{
+			V2: `{{ "2" }}`,
+		},
+		M1: map[string]any{
+			"a": map[string]any{
+				"b": `{{ "3" }}`,
+			},
+			`{{ "c" }}`: "4",
+			`{{ "d" }}`: `{{ "5" }}`,
+			`{{ "e" }}`: map[string]any{
+				"f":         `{{ "6" }}`,
+				`{{ "g" }}`: `{{ "7" }}`,
+			},
+		},
+	}
+	e := testStruct{
+		V1: "1",
+		S1: testStruct2{
+			V2: "2",
+		},
+		M1: map[string]any{
+			"a": map[string]any{
+				"b": "3",
+			},
+			"c": "4",
+			"d": "5",
+			"e": map[string]any{
+				"f": "6",
+				"g": "7",
+			},
+		},
+	}
+
+	changed, err := j2.RenderStruct(&s)
+	assert.NoError(t, err)
+	assert.True(t, changed)
+	assert.Equal(t, e, s)
+
+	changed, err = j2.RenderStruct(&s)
+	assert.NoError(t, err)
+	assert.False(t, changed)
+	assert.Equal(t, e, s)
 }
