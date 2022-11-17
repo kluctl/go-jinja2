8fdf9d834f998756f11ed6ecbbf956370ad0bb4b
Alexander Block <ablock84@gmail.com>
2022-11-17 11:22:17 +0100

    Revert "feat: Implement ignoregotemplates extension"
    This reverts commit acd7a5e4900358b5f22359521495bcd498f991fe.

--- python_src/go_jinja2/ext/__init__.py
+++ python_src/go_jinja2/ext/__init__.py
@@ -1,7 +1,5 @@
 from .kluctl_ext import KluctlExtension
 from .time_ext import TimeExtension
-from .ignore_gotemplates_ext import IgnoreGoTemplates
 
 kluctl = KluctlExtension
 time = TimeExtension
-ignoregotemplates = IgnoreGoTemplates
